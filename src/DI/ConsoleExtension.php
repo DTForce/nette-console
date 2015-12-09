a0acad91d9146c906b16bd1aba69e57a57e9f60d
Jan Mareš <jan.mares@dtforce.com>
2015-12-09 12:26:47 +0100

    Updated license terms.

--- src/DI/ConsoleExtension.php
+++ src/DI/ConsoleExtension.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * This file is part of the DTForce Nette-Console extension (http://www.dtforce.com/).
+ *
+ * This source file is subject to the GNU Lesser General Public License.
+ * For details see LICENSE.md in the root of the library.
+ */
+
 namespace DTForce\NetteConsole\DI;
 
 use DTForce\NetteConsole\Exception\ServiceNotFoundException;
